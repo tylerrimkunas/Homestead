0b06cd1eabc9b33426fc90a8c439fa72d3b025af
Tyler <tylerrimkunas@gmail.com>
2026-07-13 18:13:19 -0400

    added most layers for presentation into a web app, console app, desktop app, and web api.

---
+++ Homestead.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Homestead.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Homestead.UseCases/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Homestead.UseCases
+{
+    public class Class1
+    {
+
+    }
+}
